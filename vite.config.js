844abffbb565903e83463d9254fcd32d3f4a7de8
Arif Usman <59372093+mannarif@users.noreply.github.com>
2026-02-13 11:36:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins:[react()], server:{host:true} });
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: "all"
+  },
+  preview: {
+    allowedHosts: "all"
+  }
+});
